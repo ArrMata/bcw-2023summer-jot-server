c8ced656a0ecb605db763a6c5502bb51c2d303b4
Arrington Mata <mataarrington@gmailcom>
2023-08-23 15:23:30 -0600

    🧹 setting up ESLint, using Airbnb preset

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+	"env": {
+		"node": true,
+		"commonjs": true,
+		"es2021": true
+	},
+	"extends": [
+		"airbnb-base"
+	],
+	"parserOptions": {
+		"ecmaVersion": 15
+	},
+	"ignorePatterns": [".eslintrc.js"],
+	"rules": {
+		"linebreak-style": ["error", "windows"],
+		"indent": [2, "tab"],
+		"no-tabs": 0,
+		"no-underscore-dangle": 0,
+		"no-console": 0
+	}
+};
