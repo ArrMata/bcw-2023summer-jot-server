1f724988c0201f537247d9717b7eafb711c02159
Arrington Mata <mataarrington@gmail.com>
2023-08-23 20:24:03 -0600

    🔧 Tweaked

--- libraries/middleware.js
+++ libraries/middleware.js
@@ -6,7 +6,7 @@ const unknownEndpoint = (request, response) => {
 
 const errorHandler = (error, req, res, next) => {
 	if (error.name === 'CastError') {
-		return res.status(400).send({ error: 'malformatted id' });
+		return res.status(400).send({ error: 'Malformatted Id' });
 	}
 
 	if (error.name === 'ValidationError') {
