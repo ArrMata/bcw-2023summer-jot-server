fcbeb7753955cfdd0993e2fb2e58bd4bf01d2c1a
ArrMata <mataarrington@gmail.com>
2023-09-06 15:19:53 -0600

    changed logger to stop logging in production

--- libraries/logger.js
+++ libraries/logger.js
@@ -2,13 +2,13 @@ const { NODE_ENV } = require('./Config');
 
 class Logger {
 	static info = (...params) => {
-		if (NODE_ENV !== 'test') {
+		if (NODE_ENV !== 'test' && NODE_ENV !== 'production') {
 			console.log(...params);
 		}
 	};
 
 	static error = (...params) => {
-		if (NODE_ENV !== 'test') {
+		if (NODE_ENV !== 'test' && NODE_ENV !== 'production') {
 			console.error(...params);
 		}
 	};
